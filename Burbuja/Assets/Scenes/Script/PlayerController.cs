e3ff8ceaeba7e6ccbc7aa070ef24370dc2e6760e
kevinMG01 <montanokevin46@gmail.com>
2025-02-04 14:49:53 -0300

    rotación de player

--- Burbuja/Assets/Scenes/Script/PlayerController.cs
+++ Burbuja/Assets/Scenes/Script/PlayerController.cs
@@ -7,8 +7,14 @@ public class PlayerController : MonoBehaviour
     
     public float horizontalMove;
     public float verticalMove;
+    private Vector3 playerInput;
+
     public CharacterController player;
-    
+    private Vector3 movePlayer;
+
+    public Camera mainCamara;
+    private Vector3 camForward;
+    private Vector3 camRight;
     public float playerSpeed;
     void Start()
     {
@@ -19,11 +25,30 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * playerSpeed * Time.deltaTime);
+
     }
 
-    private void FixedUpdate() 
+    void camDirection()
     {
-        player.Move(new Vector3(horizontalMove, 0,verticalMove) * playerSpeed * Time.deltaTime);
+        camForward = mainCamara.transform.forward;
+        camRight = mainCamara.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 
 }
